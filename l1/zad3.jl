7fc7ff2ecb700733a1b3921406f25b0c5f5bdbcc
Joel Kojma <279745@student.pwr.edu.pl>
2025-10-27 00:11:19 +0100

    excercise 3 ready

--- l1/zad3.jl
+++ l1/zad3.jl
@@ -2,12 +2,28 @@
 
 function float_distribution(start_x, end_x)
 
-    println(bitstring(start_x))
-    println(bitstring(end_x))
+    next = nextfloat(start_x)
+    step = next - start_x
+    println("Start_x: ", bitstring(start_x))
+    println("Next: ", bitstring(next))
+    current = start_x
+    for i in 1:52
+        current = current + step
+        step *= 2.0
+    
+        println("Current $i: ", bitstring(current))
+    end
 
-    end_x = nextfloat(start_x)
-    println(bitstring(end_x))
+    println("End_x:      ", bitstring(end_x))
 
 end
 
+println("Dystrybucja [1.0, 2.0]")
 float_distribution(Float64(1.0), Float64(2.0))
+
+println("\nDystrybucja [0.5, 1.0]")
+float_distribution(Float64(0.5), Float64(1.0))
+
+println("\nDystrybucja [2.0, 4.0]")
+
+float_distribution(Float64(2.0), Float64(4.0))
