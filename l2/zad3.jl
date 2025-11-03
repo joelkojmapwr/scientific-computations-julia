ee7abb6d32fc6e3fb07af07f3ca6a2b1c48c2058
Joel Kojma <279745@student.pwr.edu.pl>
2025-11-03 22:21:11 +0100

    excercise 3 ready

--- l2/zad3.jl
+++ l2/zad3.jl
@@ -1,3 +1,6 @@
+using Plots
+
+
 function hilb(n::Int)
 # Function generates the Hilbert matrix  A of size n,
 #  A (i, j) = 1 / (i + j - 1)
@@ -54,10 +57,104 @@ function get_one_vector(n::Int)
     end
     return v
 end
+
+function solve_gauss(A, b)
+    return A \ b
+end
+
+function solve_inverse(A, b)
+    A_inv = inv(A)
+    return A_inv * b
+end
+
+function hilbert_experiment()
+
+    rel_errors_gauss = []
+    rel_errors_inverse = []
+    conds = []
+    for i in 1:20
+        n = i
+        A = hilb(n)
+        x = get_one_vector(n)
+        b = A * x
+
+        x_gauss = solve_gauss(A, b)
+        x_inverse = solve_inverse(A, b)
+
+        rel_gauss_error = norm(x - x_gauss, Inf)
+        rel_inverse_error = norm(x - x_inverse, Inf)
+        push!(rel_errors_gauss, rel_gauss_error)
+        push!(rel_errors_inverse, rel_inverse_error)
+
+        cond_A = cond(A, Inf)
+        push!(conds, cond_A)
+
+        println("n = $n, cond(A) = $cond_A, Error Gauss: $rel_gauss_error, Error Inverse: $rel_inverse_error")
+    end
+
+    
+    plot(1:20, rel_errors_gauss, label="Gauss Elimination", xlabel="Matrix Size (n)", ylabel="Relative Error", title="Relative Errors for Hilbert Matrix")
+    plot!(1:20, rel_errors_inverse, label="Matrix Inverse")
+    # plot(1:20, conds, label="Condition Number", xlabel="Matrix Size (n)", ylabel="Condition Number", title="Condition Numbers for Hilbert Matrix", yaxis=:log)
+    display(plot!())
+
+
+end
+
+function random_experiment()
+    n_array = [5, 10, 20]
+    c_array = [1, 10, 10e3, 10e7, 10e12, 10e16]
+    for n in n_array
+        rel_errors_gauss = []
+        rel_errors_inverse = []
+        
+        for c in c_array
+            A = matcond(n, c)
+            x = get_one_vector(n)
+            b = A * x
+
+            x_gauss = solve_gauss(A, b)
+            x_inverse = solve_inverse(A, b)
+
+            rel_gauss_error = norm(x - x_gauss, Inf)
+            rel_inverse_error = norm(x - x_inverse, Inf)
+
+            push!(rel_errors_gauss, rel_gauss_error)
+            push!(rel_errors_inverse, rel_inverse_error)
+
+            cond_A = cond(A, Inf)
+
+            println("n = $n, c = $c, cond(A) = $cond_A, Error Gauss: $rel_gauss_error, Error Inverse: $rel_inverse_error")
+        end
+        plot(c_array, rel_errors_gauss, xscale=:log10, yscale=:log10, label="Gauss Elimination", xlabel="Condition Number", ylabel="Relative Error", title="Relative Errors for Random Matrix of size $n")
+        plot!(c_array, rel_errors_inverse, label="Matrix Inverse")
+        display(plot!())
+        println("Press Enter to continue to next matrix size...")
+        readline()
+    end
+
+end
+
 n = 3
 A = hilb(n)
 println("Hilbert matrix A (3x3):")
 display(A)
 
-x = inv(get_one_vector(n))
+x = get_one_vector(n)
+display(x)
 b = A * x
+
+x_gauss = solve_gauss(A, b)
+display(x_gauss)
+
+x_inverse = solve_inverse(A, b)
+display(x_inverse)
+
+hilbert_experiment()
+
+println("Press Enter to close the plots...")
+readline()
+
+
+println("----- Random Matrix Experiment -----")
+random_experiment()
