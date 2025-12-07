6159fc4e5a2b8fea7a96bb67d99cb3e2d5987f41
Joel Kojma <279745@student.pwr.edu.pl>
2025-12-07 13:09:18 +0100

    Starting work on list 4, excercise 1 ready

---
+++ l4/ilorazy_roznicowe.jl
@@ -0,0 +1,18 @@
+module ilorazy_roznicowe
+export ilorazyRoznicowe
+
+function ilorazyRoznicowe(x::Vector{Float64}, f::Vector{Float64})
+    n = length(x)
+    fx = Vector{Vector{Float64}}()
+    fxi = copy(f)
+    fx[1] = f[1]
+    for i in 2:n
+        for j in 1:(n - i + 1)
+            fxi[j] = (fxi[j + 1] - fxi[j]) / (x[j + i - 1] - x[j])
+        end
+        fx[i] = fxi[1]
+    end
+    return fx
+end
+
+end # module
