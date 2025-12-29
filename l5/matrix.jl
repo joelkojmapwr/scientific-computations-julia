027bf7920fd2f7ed8ce77ce23075f6ee8c6a5cee
Joel Kojma <279745@student.pwr.edu.pl>
2025-12-29 18:40:08 +0100

    created function to calculate row offsets and lengths

--- l5/matrix.jl
+++ l5/matrix.jl
@@ -34,6 +34,34 @@ function calculate_column_offsets_and_lengths(n::Int, l::Int)
     return column_offsets, column_lengths
 end
 
+function calculate_row_offsets_and_lengths(n::Int, l::Int)
+    v::Int = div(n, l) # v = n / l
+    row_offsets = Vector{Int}(0, n)
+    row_lengths = Vector{Int}(0, n)
+
+    for i in 1:l
+        row_offsets[i] = 0
+        row_lengths[i] = l + i
+    end
+
+    for k in 2:(v-1)
+        row_offsets[(k-1)*l + 1] = (k-2)*l # handling first row of Bk
+        row_lengths[(k-1)*l + 1] = 2*l + 1
+        for i in 2:l
+            row_offsets[(k-1)*l + i] = (k-1)*l - 1
+            row_lengths[(k-1)*l + i] = l + i + 1 # Ak + Ck + Bk
+        end
+    end
+
+    row_offsets[(v-1)*l + 1] = (v-2)*l
+    row_lengths[(v-1)*l + 1] = 2 * l + 1
+    for i in 2:l
+        row_offsets[(v-1)*l + i] = (v-2)*l - 1
+        row_lengths[(v-1)*l + i] = l + 1 # Ak + Bk
+    end
+
+    return row_offsets, row_lengths
+end
 
 
 struct SparseMatrix
